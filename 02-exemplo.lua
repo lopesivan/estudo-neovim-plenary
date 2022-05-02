f4339ce5c74e2997c126132c1be5899cfd12b40f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-02 01:12:03 -0300

    Adiciona o arquivo 02-exemplo.lua

---
+++ 02-exemplo.lua
@@ -0,0 +1,17 @@
+-- programa em lua
+local Job = require "plenary.job"
+
+local results = {}
+
+Job:new({
+	command = 'bash',
+	args = { "-c", "'gcc -c hello.c'" },
+	cwd = "/workspace/pasta1",
+	env = { ['a'] = 'b' },
+	on_stdout = function(_, line)
+		print "first job"
+		table.insert(results, line)
+	end,
+}):sync() -- or start()
+
+print(vim.inspect.inspect(results))

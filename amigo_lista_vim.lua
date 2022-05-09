d958e11a84ffe11d692a5567e0ffa97c5173292c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-09 09:07:24 -0300

    Atualiza o arquivo amigo_lista_vim.lua

--- amigo_lista_vim.lua
+++ amigo_lista_vim.lua
@@ -1,6 +1,8 @@
 
+-- local filename = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), " ")
 --[[
-local filename = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), " ")
+pessoal, como converto para lua essa linha de comando ???
+call jobstart('nvim -h', {'on_stdout':{j,d,e->append(line('.'),d)}})
 ]]
 vim.fn.jobstart(‘nvim -h’, {
 	on_stdout = function(j, d, e)
